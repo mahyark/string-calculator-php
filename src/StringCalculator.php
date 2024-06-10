10707b6e8efbbce3dec3cfc8408275d97e9de1ac
Mahyar Kian <mahyar@kings-agency.com>
2024-06-10 15:12:38 +0200

    Added methods and tests

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -2,7 +2,103 @@
 
 namespace Deg540\StringCalculatorPHP;
 
+use Exception;
+
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+    private int $calledCount = 0;
+
+    public function GetCalledCount(): int
+    {
+        return $this->calledCount;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function Add(string $numbers): int
+    {
+        $sum = 0;
+        $this->calledCount++;
+        $numbersArray = self::getNumbersArray($numbers);
+
+        if (!self::hasNegativeNumbers($numbersArray)) {
+            foreach ($numbersArray as $number) {
+                $sum += self::getFormattedNumber($number);
+            }
+        }
+
+        return $sum;
+    }
+
+    private function getNumbersArray($numbers): array
+    {
+        $delimiterAndNumbers = self::getDelimiterAndNumbers($numbers);
+        $delimiter = $delimiterAndNumbers['delimiter'];
+
+        if (self::hasCustomDelimiter($numbers)) {
+            $numbers = $delimiterAndNumbers['numbers'];
+        }
+
+        return array_map('intval', preg_split($delimiter, $numbers));
+    }
+
+    private function getDelimiterAndNumbers(string $numbers): array
+    {
+        $delimiter = '/[\n,]/';
+
+        if (self::hasCustomDelimiter($numbers)) {
+            $customDelimiters = str_replace('//', '', preg_split('/\n/', $numbers)[0]);
+            $numbers = str_replace("//$customDelimiters", "", $numbers);
+            $customDelimitersArray = self::getCustomDelimitersArray($customDelimiters);
+            $delimiter = "/[" . implode("", $customDelimitersArray) . "]/";
+        }
+
+        return [
+            'delimiter' => $delimiter,
+            'numbers' => $numbers
+        ];
+    }
+
+    private function hasCustomDelimiter($numbers): bool
+    {
+        $customDelimiter = preg_split('/\n/', $numbers)[0];
+        if (str_starts_with($customDelimiter, '//')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getCustomDelimitersArray($customDelimiter): array
+    {
+        $trimmedString = trim($customDelimiter, '[]');
+        return explode('][', $trimmedString);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function hasNegativeNumbers($numbersArray): bool
+    {
+        $negativeNumbers = array();
+
+        foreach ($numbersArray as $number) {
+            if ($number < 0) {
+                $negativeNumbers[] = $number;
+            }
+        }
+
+        if (count($negativeNumbers) > 0) {
+            $negativeNumbersOutput = implode(', ', $negativeNumbers);
+                throw new Exception("negatives not allowed - \"{$negativeNumbersOutput}\"");
+        }
+
+        return false;
+    }
+
+    private function getFormattedNumber(int $number): int
+    {
+        return (int) substr($number, -3);
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -5,9 +5,162 @@ declare(strict_types=1);
 namespace Deg540\StringCalculatorPHP\Test;
 
 use Deg540\StringCalculatorPHP\StringCalculator;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    private StringCalculator $stringCalculator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+
+
+    /**
+     * @test
+     */
+    public function returnsZeroSumWhenInputEmpty()
+    {
+        $results = $this->stringCalculator->Add('');
+        $this->assertEquals(0, $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsGivenNumberWhenOneNumberGiven()
+    {
+        $number = 10;
+
+        $results  = $this->stringCalculator->Add("{$number}");
+
+        $this->assertEquals($number, $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumWhenTwoNumbersGiven()
+    {
+        $number1 = 5;
+        $number2 = 5;
+
+        $results  = $this->stringCalculator->Add("{$number1}, {$number2}");
+
+        $this->assertEquals($number1 + $number2, $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumWhenUnknownAmountOfNumbersGiven()
+    {
+        $numbers = [5, 10, 15, 20];
+
+        $results  = $this->stringCalculator->Add("{$numbers[0]}, {$numbers[1]}, {$numbers[2]}, {$numbers[3]}");
+
+        $this->assertEquals(array_sum($numbers), $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumWhenNewLineInNumbersGiven()
+    {
+        $numbers = [5, 10, 15];
+
+        $results  = $this->stringCalculator->Add("{$numbers[0]}\n {$numbers[1]}, {$numbers[2]}");
+
+        $this->assertEquals(array_sum($numbers), $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumOfGivenNumbersWithCustomDelimiter()
+    {
+        $numbers = [5, 10];
+        $customDelimiter = ';';
+
+        $results  = $this->stringCalculator->Add("//{$customDelimiter}\n{$numbers[0]}{$customDelimiter} {$numbers[1]}");
+
+        $this->assertEquals(array_sum($numbers), $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsExceptionWithNegativeGivenNumber()
+    {
+        $numbers = [5, -10];
+
+        $this->expectException(Exception::class);
+
+        $this->stringCalculator->Add("{$numbers[0]}, {$numbers[1]}");
+    }
+
+    /**
+     * @test
+     */
+    public function returnsExceptionWithAllNegativeGivenNumbers()
+    {
+        $numbers = [-5, 10, -20];
+
+        $this->expectException(Exception::class);
+
+        $this->stringCalculator->Add("{$numbers[0]}, {$numbers[1]}, {$numbers[2]}");
+    }
+
+    /**
+     * @test
+     */
+    public function returnsHowManyTimesAddMethodWasInvoked()
+    {
+        $results = $this->stringCalculator->GetCalledCount();
+        $this->assertEquals(0, $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumWhenAGivenNumberAboveThousand()
+    {
+        $numbers = [5, 1010, 15];
+        $expectedSum = 30;
+
+        $results  = $this->stringCalculator->Add("{$numbers[0]}\n {$numbers[1]}, {$numbers[2]}");
+
+        $this->assertEquals($expectedSum, $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumOfGivenNumbersWithLongCustomDelimiter()
+    {
+        $numbers = [5, 10];
+        $customDelimiter = '***';
+
+        $results  = $this->stringCalculator->Add("//[{$customDelimiter}]\n{$numbers[0]}{$customDelimiter} {$numbers[1]}");
+
+        $this->assertEquals(array_sum($numbers), $results);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSumOfGivenNumbersWithMultipleCustomDelimiter()
+    {
+        $numbers = [1, 2, 3];
+        $customDelimiters = ['***', '%%%'];
+
+        $results  = $this->stringCalculator->Add(
+            "//[{$customDelimiters[0]}][{$customDelimiters[1]}]\n{$numbers[0]}{$customDelimiters[0]} {$numbers[1]}{$customDelimiters[1]} {$numbers[2]}"
+        );
+
+        $this->assertEquals(array_sum($numbers), $results);
+    }
 }
